21ff480c99f31371f1995870a096ac7033870879
JainishaJain-09 <jainisha1100.becse24@chitkara.edu.in>
2026-06-28 19:52:30 +0530

    Time: 10 ms (29.58%), Space: 77.4 MB (61.67%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        arr[0]=1;
+        for(int i=1;i<arr.length;i++){
+            arr[i]=Math.min(arr[i],arr[i-1]+1);
+        }
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+}
