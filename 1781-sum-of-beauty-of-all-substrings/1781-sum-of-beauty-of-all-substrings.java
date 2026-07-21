d05fbccba0221647e4913027cd3bd08986f9dfcf
JainishaJain-09 <jainisha1100.becse24@chitkara.edu.in>
2026-07-21 17:27:32 +0530

    Time: 76 ms (40.37%), Space: 44.2 MB (59.8%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,29 +1,55 @@
+// class Solution {
+//     public int beautySum(String s) {
+//         int count=0;
+//         for (int i=0;i<s.length();i++) {
+//             HashMap<Character, Integer> map = new HashMap<>();
+//             for(int j=i;j<s.length();j++) {
+//                 char ch=s.charAt(j);
+//                 if(map.containsKey(ch)) {
+//                     map.put(ch,map.get(ch)+1);
+//                 } 
+//                 else {
+//                     map.put(ch,1);
+//                 }
+//                 int max=Integer.MIN_VALUE;
+//                 int min=Integer.MAX_VALUE;
+//                 for(int val:map.values()) {
+//                     if(val>max) {
+//                         max=val;
+//                     }
+//                     if(val<min) {
+//                         min=val;
+//                     }
+//                 }
+//                 count=count+(max-min);
+//             }
+//         }
+//         return count;
+//     }
+// }
 class Solution {
     public int beautySum(String s) {
-        int count=0;
-        for (int i=0;i<s.length();i++) {
-            HashMap<Character, Integer> map = new HashMap<>();
-            for(int j=i;j<s.length();j++) {
-                char ch=s.charAt(j);
-                if(map.containsKey(ch)) {
-                    map.put(ch,map.get(ch)+1);
-                } 
-                else {
-                    map.put(ch,1);
-                }
-                int max=Integer.MIN_VALUE;
-                int min=Integer.MAX_VALUE;
-                for(int val:map.values()) {
-                    if(val>max) {
-                        max=val;
-                    }
-                    if(val<min) {
-                        min=val;
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int[] freq = new int[26];
+            for (int j = i; j < s.length(); j++) {
+                freq[s.charAt(j) - 'a']++;
+                int max = 0;
+                int min = Integer.MAX_VALUE;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        if (freq[k] > max) {
+                            max = freq[k];
+                        }
+                        if (freq[k] < min) {
+                            min = freq[k];
+                        }
                     }
                 }
-                count=count+(max-min);
+                ans += (max - min);
             }
         }
-        return count;
+
+        return ans;
     }
 }
