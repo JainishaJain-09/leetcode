1900b33f2df6a0f9d4332bce7fbf0d5f6fe5c0f7
JainishaJain-09 <jainisha1100.becse24@chitkara.edu.in>
2026-07-25 17:08:37 +0530

    Time: 1 ms (100%), Space: 42.5 MB (74.48%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int n) {
+        int max=0;
+        int smax=0;
+        while(n>0){
+            if((n%10)>=max){
+                smax=max;
+                max=n%10;
+            }
+            else if((n%10)!=max && (n%10)>smax){
+                smax=n%10;
+            }
+            n/=10;
+        }
+        return max*smax;
+
+    }
+}
