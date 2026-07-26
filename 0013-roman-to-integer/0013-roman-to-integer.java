58ac578480e09d129fb8be6023b85e9270510c9b
JainishaJain-09 <jainisha1100.becse24@chitkara.edu.in>
2026-07-26 18:55:11 +0530

    Time: 2 ms (100%), Space: 46.8 MB (20.9%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,24 +1,47 @@
 class Solution {
     public int romanToInt(String s) {
-        int sum=0;
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            map.put('I',1);
-            map.put('V',5);
-            map.put('X',10);
-            map.put('L',50);
-            map.put('C',100);
-            map.put('D',500);
-            map.put('M',1000);
-        }
-        for(int i=0;i<s.length()-1;i++){
-            if(map.get(s.charAt(i))<map.get(s.charAt(i+1))){
-                sum-=map.get(s.charAt(i));
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'I') {
+                if (i + 1 < s.length() && s.charAt(i + 1) == 'V') {
+                    result += 4;
+                    i++;
+                } else if (i + 1 < s.length() && s.charAt(i + 1) == 'X') {
+                    result += 9;
+                    i++;
+                } else {
+                    result += 1;
+                }
+            } else if (s.charAt(i) == 'X') {
+                if (i + 1 < s.length() && s.charAt(i + 1) == 'L') {
+                    result += 40;
+                    i++;
+                } else if (i + 1 < s.length() && s.charAt(i + 1) == 'C') {
+                    result += 90;
+                    i++;
+                } else {
+                    result += 10;
+                }
+            } else if (s.charAt(i) == 'C') {
+                if (i + 1 < s.length() && s.charAt(i + 1) == 'D') {
+                    result += 400;
+                    i++;
+                } else if (i + 1 < s.length() && s.charAt(i + 1) == 'M') {
+                    result += 900;
+                    i++;
+                } else {
+                    result += 100;
+                }
+            } else if (s.charAt(i) == 'V') {
+                result += 5;
+            } else if (s.charAt(i) == 'L') {
+                result += 50;
+            } else if (s.charAt(i) == 'D') {
+                result += 500;
+            } else if (s.charAt(i) == 'M') {
+                result += 1000;
             }
-            else{
-                sum+=map.get(s.charAt(i));
-            }
         }
-        return sum+map.get(s.charAt(s.length()-1));
+        return result;
     }
 }
