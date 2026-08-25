3cd596126639fa892e144075e18aa1cc92d18c05
JainishaJain-09 <jainisha1100.becse24@chitkara.edu.in>
2026-08-25 17:17:23 +0530

    Time: 15 ms (59.96%), Space: 51.8 MB (6.45%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,16 +1,33 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        // Arrays.sort(g);
+        // Arrays.sort(s);
+        // int count = 0;
+        // for(int i=0;i<g.length;i++) {
+        //     for(int j=0;j<s.length;j++) {
+        //         if(g[i] <= s[j]) {
+        //             count++;
+        //             s[j] = -1;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        // using greedy 
         Arrays.sort(g);
         Arrays.sort(s);
-        int count = 0;
-        for(int i=0;i<g.length;i++) {
-            for(int j=0;j<s.length;j++) {
-
-                if(g[i] <= s[j]) {
-                    count++;
-                    s[j] = -1;
-                    break;
-                }
+        int i=0;
+        int j=0;
+        int count=0;
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i]){
+                count++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
             }
         }
         return count;
